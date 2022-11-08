60dbd10dd3e207ec5b6197eefa292913bb517708
TigerRUS <qwerasdfgzxcv20@gmail.com>
2022-11-08 16:40:49 +0500

    commit

--- task3/script.js
+++ task3/script.js
@@ -1,1 +1,17 @@
 let num = parseInt(prompt("Введите любое натуральное число"));
+
+let arr = [];
+for(let i = 0; i <= 30; i++)
+{
+   arr[i] = (2**(i - 1)*(2**i - 1));
+}
+
+if(num % 2 == 0)
+{
+   if(arr.includes(num))
+   {
+      alert(num + " является совершенным");
+   }
+   else alert(num + " не является совершенным");
+}
+else alert(num + " не является совершенным");
---
+++ task4/script.js
@@ -0,0 +1,29 @@
+const points = {
+   'Ида': 37,
+   'Лена': 46,
+   'Артур': 70,
+   'Платон': 88,
+};
+const marks = {
+   'Ида': 37,
+   'Лена': 46,
+   'Артур': 70,
+   'Платон': 88,
+};
+
+for (let x in points) {
+   if (points[x] < 60) marks[x] = "незачёт";
+   else if (points[x] < 70) marks[x] = 3;
+   else if (points[x] < 90) marks[x] = 4;
+   else marks[x] = 5;
+}
+
+let points_primer = document.getElementById('points_out');
+for (let x in points) {
+   points_primer.innerHTML += x + ' : ' + points[x] + '<br> ';
+}
+
+let marks_primer = document.getElementById('marks_out');
+for (let x in points) {
+   marks_primer.innerHTML += x + ' : ' + marks[x] + '<br> ';
+}
