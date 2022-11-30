ab27e49579eba4ce14303a20859f48e1a9a71e61
TigerRUS <qwerasdfgzxcv20@gmail.com>
2022-11-30 17:22:26 +0500

    commit

--- script.js
+++ script.js
@@ -3,32 +3,33 @@ function printTable() {
    if ((Object.keys(basket)).length != 0)
       table += '<table>' + '<td>' + "Товар" + '</td>' + '<td>' + "<td>" + "Цена"
    for (let x in basket) {
-      let inner = `<div class="bin" onclick="deleteFromBasket('${x}', ${idx[x]});"><img class="bin_img" src="images/garbage.png" alt=""></div>`
+      let inner = `<div class="bin" onclick="deleteFromBasket('${x}', ${idx[x]}, ${prices[x]});"><img class="bin_img" src="images/garbage.png" alt=""></div>`
       table += `<tr  class="td_color">`
       table += '<td>' + x + '</td>'
       table += '<td>'
       table += '<td>' + basket[x] + ' руб.' + '</td>'
-      if(x in activeCheckboxes)
-         table += '<td>' + `<input type="checkbox" class="checkbox" id="${x}" onclick="check();" value="${basket[x]}" checked />` + '</td>'
+      if(prices[x] in activeCheckboxes)
+         table += '<td>' + `<input type="checkbox" class="checkbox" id="${prices[x]}" onclick="check();" value="${basket[x]}" checked />` + '</td>'
       else
-         table += '<td>' + `<input type="checkbox" class="checkbox" id="${x}" onclick="check();" value="${basket[x]}">` + '</td>'
+         table += '<td>' + `<input type="checkbox" class="checkbox" id="${prices[x]}" onclick="check();" value="${basket[x]}">` + '</td>'
       table += '<td>' + inner + '</td>'
    }
    document.getElementById('basket_out').innerHTML = table + '</table>';
+   cost()
 }
 
 function addToBasket(s) {
    basket[s] = prices[s]
    idx[s] = this.event.target.id
-   printTable(this.event.target.id)
+   printTable()
    document.getElementById(this.event.target.id).style = "opacity: 30%"
 }
 
-function deleteFromBasket(x, idx) {
+function deleteFromBasket(x, idx, id) {
    delete basket[x]
    document.getElementById(idx).style = "opacity: 100%"
    check()
-   delete activeCheckboxes[x]
+   delete activeCheckboxes[id]
    printTable()
    if ((Object.keys(basket)).length == 0) {
       document.getElementById('price').style = "border: 0px solid #a30101;"
@@ -36,20 +37,20 @@ function deleteFromBasket(x, idx) {
 }
 
 function check(){
-   let a = document.querySelectorAll('input:checked');
-   let sum = 0
+   let a = document.querySelectorAll('.checkbox');
    for (var i = 0; i < a.length; i++) {
-      if(!(a[i].id in basket) && a[i].id in activeCheckboxes)
-      {
-         delete activeCheckboxes[a[i].id]
-         $(`#${a[i].id}`).prop('check', false)
-      }
-      else
-      {
+      if($(`#${a[i].id}`).prop("checked"))
          activeCheckboxes[a[i].id] = a[i].id;
-         sum += Number(a[i].value)
-      }
+      else
+         delete activeCheckboxes[a[i].id]
    }
+   printTable()
+}
+
+function cost(){
+   let sum = 0
+   for(let x in activeCheckboxes)
+      sum += Number(x)
    if(sum != 0)
       document.getElementById('price_out').innerHTML = "<table>" + `<td class="items_price" id="price">Сумма заказа: ${sum} руб.</td>` + "</table>";
    else 
